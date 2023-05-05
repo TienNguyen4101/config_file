070d132a19941aa8679da578b11ee081da662f80
TienNguyen4101 <92602519+TienNguyen4101@users.noreply.github.com>
2023-05-05 11:49:33 +0700

    Update start_service.sh

--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-service nginx start
-service mariadb start
-service php7.4-fpm start
+
+# Start the first process
+service nginx start &
+  
+# Start the second process
+service mariadb start &
+  
+service php7.4-fpm start &
+# Wait for any process to exit
+wait -n
+  
+# Exit with status of process that exited first
+exit $?
